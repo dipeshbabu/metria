f62589be9f5f87fbcc5cafd125b75f2600e1f236
Dipesh Babu <59379458+dipeshbabu@users.noreply.github.com>
2026-08-06 22:56:44 -0400

    Expose trajectory measurement bridge

---
+++ src/metria/measurements/__init__.py
@@ -0,0 +1,5 @@
+"""Built-in Metria measurement protocols."""
+
+from .trajectory import TokenTrajectoryProtocol, compare_trajectory_results
+
+__all__ = ["TokenTrajectoryProtocol", "compare_trajectory_results"]
